7bc3e687eaaf134d9c973733c067d6b400327677
Flat-Pixels <steeveverdieu@gmail.com>
2018-12-26 16:45:52 -0500

    refactor: Add comments to the index page
    Make some examples of the configuration of the router into the index
    page.

--- index.php
+++ index.php
@@ -1,38 +1,45 @@
 <?php
-
 require __DIR__ . '/vendor/autoload.php';
 
+/*
+* Examples of routes registering
+* [1] : Get an instance of the router
+* [2] : Register the route into the route
+* [3] : Resolve the current user request
+*/
+
+//[1]
+
+//Create an instance of the router
 $router = LightRoute\LightRouter::getInstance();
 
+//[2]
 
-
-
-//On enregistre les differentes routes
+//Routes to call in GET without any params
 $router->addRoute('get', '/', function(){
     echo "Welcome to the home page";
 });
-
 
 $router->addRoute('get', '/posts/', function(){
     echo "Welcome to the posts page";
 });
 
+//Route to call in GET with params
 $router->addRoute('get', '/post/:id/:slug', function( $id, $slug ){
-    echo "Afficher le post ayant l'id: " . $id;
-    echo "Afficher le post ayant le slug: " . $slug;
-}, 'post_id')->format(['id' => "[0-8]"]);
+    echo "Post id: " . $id;
+    echo "Post slug: " . $slug;
+}, 'single_post')->format(['id' => "[0-8]"]);
 
-$router->addRoute('get', '/go', function() use($router){
-    echo "Redirecting...";
-    $router->redirect('POST_ID', ['slug' => 'la ferme', 'id' => 4]);
-})->format(['id' => "[0-8]"]);
-
+//Route to call in POST
 $router->addRoute('post', '/post/add', function(){
-    echo "Post variable there:";
     var_dump( $_POST );
 });
 
-$router->resolve();
+//Route redirects to another route
+$router->addRoute('get', '/go', function() use($router){
+    $router->redirect('single_post', ['slug' => 'la ferme', 'id' => 4]);
+})->format(['id' => "[0-8]"]);
 
-?>
+//[3]
+$router->resolve();
 
